0aa19db7672442973cccddfd8518b34beb18cb4f
Leo Sell <leosell14@gmail.com>
2023-02-05 00:46:50 -0300

    rota da empresa para get, delete e put

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,9 @@ import { userGet } from "./rotaGet/userGet.js"
 import { userDelete } from "./rotaDelete/userDelete.js"
 import { userUpdate } from "./rotaUpdate/userUpdate.js"
 import { empresaPost } from "./rotaPost/empresaPost.js"
+import { empresaGet } from "./rotaGet/empresaGet.js"
+import { empresaDelete } from "./rotaDelete/empresaDelete.js"
+import { empresaUpdate } from "./rotaUpdate/empresaUpdate.js"
 
 export const routes = express.Router()
 
@@ -13,3 +16,6 @@ routes.use('/user/deletar', userDelete)
 routes.use('/user/update', userUpdate)
 
 routes.use('/empresa', empresaPost)
+routes.use('/empresa/busca', empresaGet)
+routes.use('/empresa/deletar', empresaDelete)
+routes.use('/empresa/update', empresaUpdate)
