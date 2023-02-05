6e3add890a904bb29e5a2abd51dade3c9689b586
Leo Sell <leosell14@gmail.com>
2023-02-05 00:47:53 -0300

    url para buscar dados da empresa

---
+++ routes/rotaGet/empresaGet.js
@@ -0,0 +1,22 @@
+import express from "express"
+import { Empresa } from "../../dataBase/models/Empresa.js"
+
+export const empresaGet = express.Router()
+
+empresaGet.get('/cadastros', async (req, res) => {
+    const page = req.query.page || 1
+    const limit = 10
+    const offSet = (page - 1) * limit
+    const codNova = await Empresa.findAll({
+        limit,
+        offSet
+    }).catch((err) => console.log(err))
+
+    if (codNova) {
+        return res
+            .status(200)
+            .json({ codNova })
+    } else {
+        return res.send({ message: 'Empresa não cadastada' })
+    }
+})
