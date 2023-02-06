e4037799265e5eb606e62916522931cd8106ad6f
Leo Sell <leosell14@gmail.com>
2023-02-06 00:19:39 -0300

    url ajeitadas para facilitar na hora de pedir as requisicoes

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,4 @@
 import express from "express"
-import jwt from "jsonwebtoken"
 import { userPost } from "./rotaPost/userPost.js"
 import { userGet } from "./rotaGet/userGet.js"
 import { userDelete } from "./rotaDelete/userDelete.js"
@@ -12,28 +11,14 @@ import { loginPost } from "./rotaPost/loginPost.js"
 
 export const routes = express.Router()
 
-const checkToken = (req, res, next) => {
-    try {
-        const token = req.headers.authorization.split(' ')[1]
-        const decoded = jwt.verify(token, process.env.CHAVE_CRIPTOGRAFICA)
-        req.userData = decoded
-        next()
-    } catch (err) {
-        console.log(err)
-        return res
-            .status(401)
-            .json({ message: 'Usuário sem token valido' })
-    }
-}
-
 routes.use('/', loginPost)
 
-routes.use('/user/cadastro', checkToken, userPost)
-routes.use('/user/busca', checkToken, userGet)
-routes.use('/user/deletar', checkToken, userDelete)
-routes.use('/user/update', checkToken, userUpdate)
+routes.use('/user/cadastro', userPost)
+routes.use('/user/busca', userGet)
+routes.use('/user/deletar', userDelete)
+routes.use('/user/update', userUpdate)
 
-routes.use('/empresa/cadastro', checkToken, empresaPost)
-routes.use('/empresa/busca', checkToken, empresaGet)
-routes.use('/empresa/deletar', checkToken, empresaDelete)
-routes.use('/empresa/update', checkToken, empresaUpdate)
+routes.use('/empresa/cadastro', empresaPost)
+routes.use('/empresa/busca', empresaGet)
+routes.use('/empresa/deletar', empresaDelete)
+routes.use('/empresa/update', empresaUpdate)
