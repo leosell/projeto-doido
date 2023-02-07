bf924ef89908bb941ef2d21dd9869d893c939112
Leonardo Sell <leosell14@gmail.com>
2023-02-07 19:23:48 -0300

    add URL do senai

--- App/src/API/index.js
+++ App/src/API/index.js
@@ -3,7 +3,8 @@ import axios from "axios"
 
 const API = axios.create({
     baseURL: 'http://10.100.106.109:4000',
-    baseURL: 'http://172.20.10.10:4000'
+    baseURL: 'http://172.20.10.10:4000',
+    baseURL: 'http://172.20.60.171:4000'
 })
 
 export default API
