f0cf2ff4ab49f784148414cecb121ac1d2610d69
Leonardo Sell <leosell14@gmail.com>
2023-02-07 18:54:37 -0300

    URL para nova e iphone Leo

--- App/src/API/index.js
+++ App/src/API/index.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-dupe-keys */
 import axios from "axios"
 
 const API = axios.create({
-    baseURL: 'http://192.168.1.14:4000'
+    baseURL: 'http://10.100.106.109:4000',
+    baseURL: 'http://172.20.10.10:4000'
 })
 
 export default API
