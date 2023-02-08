e02b09de8cc79b77b01f8a03c2488a366e144974
Leo Sell <leosell14@gmail.com>
2023-02-08 03:02:35 -0300

    validando rotas para envio ao front

--- App/src/App.js
+++ App/src/App.js
@@ -1,41 +1,29 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React from "react"
+import React, { useContext } from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+
 import Login from "./pages/Login"
-
-// import { Context, inicialState } from "./source/status"
-
+import { Context, Provider } from "./source/status"
 import Users from "./pages/Users/index"
 import Empresas from "./pages/Empresas/index"
 
-import { Provider } from "./source/status"
-
 const App = () => {
 
-    // const { state = inicialState } = useContext(Context)
+    const { state } = useContext(Context)
 
     return (
         <BrowserRouter>
             <Routes>
-                {/* { state.isLogged ? (
+                <Route path="/" element={ <Login /> } />
+                { state.isLogged ? (
                     <>
-                        <Route 
-                            path="/"
-                            element={ <Users /> }
-                            loader={async ({ params }) => {
-                                return fetch(
-                                    `/user/busca`
-                                )
-                            }}
-                        />
+                        <Route path="/users" element={ <Users /> } />
                         <Route path="/empresas" element={ <Empresas /> } />
                     </>
-                ) : (
-                    <Route path="/login" element={ <Login /> } />
-                ) } */}
-                <Route path="/" element={ <Login /> } />
+                ) : (null)}
+                {/* <Route path="/" element={ <Login /> } />
                 <Route path="/users" element={ <Users /> } />
-                <Route path="/empresas" element={ <Empresas /> } />
+                <Route path="/empresas" element={ <Empresas /> } /> */}
             </Routes>
         </BrowserRouter>
     )
