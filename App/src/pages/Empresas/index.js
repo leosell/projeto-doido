2d9a9f12925babd4d16d04abbbb89d6b85edb838
Leonardo Sell <leosell14@gmail.com>
2023-02-07 23:56:46 -0300

    table de busca sendo criada

--- App/src/pages/Empresas/index.js
+++ App/src/pages/Empresas/index.js
@@ -1,8 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import swal from "sweetalert2"
+import API from "../../API";
 
 const Empresas = () => {
+
+    const [ empresa, setEmpresa ] = useState([])
+
+    useEffect( () => {
+        const empresaGet = async () => {
+            const result = await API.get('/empresa/busca')
+            console.log(result.data)
+            setEmpresa(result.data)
+        }
+        empresaGet()
+    }, [])
+
     return (
-        <h1>Empresas</h1>
+        <div>
+            <label>Filtrar por Nome ou Código Nova</label>
+            <input
+                type="text"
+                // onBlur={(event) => {
+                //     const search = event.target.value
+                //     const resultSearch = empresa.filter(empresaSearch => empresaSearch.nameEmpresa.includes(search))
+                //     setEmpresa(resultSearch)
+                // }}
+            />
+            <button
+                onClick={() => {
+                    swal.fire({
+                        toast: true,
+                        position: 'top-end',
+                        icon: 'info',
+                        showConfirmButton: false,
+                        timer: 3000,
+                        timerProgressBar: true,
+                        text: 'Em desenvolvimento...'
+                    })
+                }}
+            >
+                Adicionar Empresa
+            </button>
+
+            <div>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>Ativo</th>
+                            <th>Cód.Nova</th>
+                            <th>Nome</th>
+                            <th>Regional</th>
+                            <th>Comercial</th>
+                            <th>Ações</th>
+                        </tr>
+                        {empresa.map(list => (
+                            <tr key={list.id}>
+                                <td>{list.active}</td>
+                                <td>{list.codNova}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
     )
 }
 
